b5fe547a352dbe060f344140ccb21e3d62a4e811
justoll <131144712+justoll@users.noreply.github.com>
2023-04-18 11:08:35 +0200

    Add files via upload
    Initial commit

---
+++ src/DetectTextEncoding.d
@@ -0,0 +1,11 @@
+DetectTextEncoding.o: src/DetectTextEncoding.c \
+ ../libDetectSQLEncoding/include/DetectTextEncoding.h \
+ ../icu/include/unicode/ucnv.h ../icu/include/unicode/ucnv_err.h \
+ ../icu/include/unicode/utypes.h ../icu/include/unicode/umachine.h \
+ ../icu/include/unicode/ptypes.h ../icu/include/unicode/platform.h \
+ ../icu/include/unicode/uconfig.h ../icu/include/unicode/uvernum.h \
+ ../icu/include/unicode/urename.h ../icu/include/unicode/uversion.h \
+ ../icu/include/unicode/utf.h ../icu/include/unicode/utf8.h \
+ ../icu/include/unicode/utf16.h ../icu/include/unicode/utf_old.h \
+ ../icu/include/unicode/uenum.h ../icu/include/unicode/ucsdet.h \
+ ../icu/include/unicode/uclean.h
